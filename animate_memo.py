5fca492aab1c91830d78f841d5358f794bb4a6f0
GuiBrandt <gui.g.brandt@gmail.com>
2018-11-17 13:36:36 -0200

    Agora tá lindo

--- animate_memo.py
+++ animate_memo.py
@@ -1,6 +1,5 @@
 import io
 import matplotlib.pyplot as plt
-import numpy as np
 
 from time import sleep
 
@@ -21,15 +20,17 @@ if __name__ == "__main__":
     accesses = []
     for _ in range(len(a)):
         accesses.append([0] * len(b))
-    max_accesses = 0
+
+    normal = plt.Normalize(-1, 4)
 
     while True:
         step += 1
-        cell_text = []
         
         line = file.readline()
         if line == "":
             break
+
+        cell_text = []
 
         m, n = map(int, line.split(" "))
 
@@ -37,16 +38,10 @@ if __name__ == "__main__":
             if m > 0 and n > 0:
                 accesses[m - 1][n - 1] += 1
 
-            acc = accesses[m - 1][n - 1]
-            max_accesses = max_accesses if max_accesses > acc else acc
-
         else:
             for tp in [(m - 1, n), (m, n - 1)]:
                 if tp[0] >= 0 and tp[1] >= 0:
                     accesses[tp[0]][tp[1]] += 1
-
-                    acc = accesses[tp[0]][tp[1]]
-                    max_accesses = max_accesses if max_accesses > acc else acc
 
         line = file.readline().strip("\r\n")
         while line != "":
@@ -70,31 +65,49 @@ if __name__ == "__main__":
 
             for c, cell in table.get_celld().items():
                 if c[0] > 0 and c[1] >= 0:
-                    cell.get_text().set_color("white")
-                    cell.set_fill(True)                    
+                    cell.set_fill(True)
 
-        else:
-            normal = plt.Normalize(-1, max_accesses + 1)
-            colors = plt.cm.viridis(normal(accesses))
+        colors = plt.cm.viridis(normal(accesses))
+        cells = table.get_celld()
+        current_cell = cells[m + 1, n]
 
-            for c, cell in table.get_celld().items():
-                i, j = c
+        last_fc = current_cell.get_fc()
+        last_tc = current_cell.get_text().get_color()
 
-                if i > 0 and j >= 0:
-                    cell.get_text().set_text(cell_text[i - 1][j])
+        for c in [(m, n), (m - 1, n - 1), (m - 1, n), (m, n - 1)]:
+            i, j = c
 
-                    if i - 1 == m and j == n:
-                        cell.set_fc("red")
-                    else:
-                        cell.set_fc(colors[i - 1][j])
+            if i < 0 or j < 0:
+                continue
+
+            cell = cells[i + 1, j]
+            cell.get_text().set_text(cell_text[i][j])
+
+            if cell == current_cell:
+                cell.set_fc("red")
+                cell.get_text().set_color("white")
+            else:
+                cell.set_fc(colors[i][j])
+                cell.get_text().set_color("white")
 
         if caption is None:
-            caption = plt.text(0, 1.1, "", fontsize=18)
+            caption = plt.text(0, 1.1, "", fontsize=16)
 
-        caption.set_text('Step: %d' % (step))
+        caption.set_text('Step: %d      m: %d       n: %d' % (step, m, n))
         
         plt.draw()
         plt.pause(0.01)
 
+        current_cell.set_fc(last_fc)
+        current_cell.get_text().set_color(last_tc)
+
     plt.ioff()
+    caption = plt.text(
+        0, -0.1, 
+        "Finished: LCS is \"%s\", %d memo accesses" % (
+            cell_text[len(a) - 1][len(b) - 1],
+            sum([item for sub in accesses for item in sub])), 
+            fontsize=16
+        )
+
     plt.show()
